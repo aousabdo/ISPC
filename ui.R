c2a43f23eb0146f94cc9a49c0afd118b8b23fdba
aousabdo <aousabdo@gmail.com>
2014-04-29 14:47:35 -0400

    Added Greek letters to ui.R

--- ui.R
+++ ui.R
@@ -7,13 +7,13 @@ shinyUI(pageWithSidebar(
   # Application title
   headerPanel(    HTML(
     '<div id="stats_header">
-    	Statistical-Power Interactive Calculator
+    	Interactive Statistical-Power Calculator
     <a href="http://www.statstudio.net/free-tools/" target="_blank">
     <img id="statstudio_logo" align="right" alt="StatStudio Logo" src="./StatStudio_Logo.png", height = 72, width = 72>
     </a>
     </div>'
   ),
-  "Statistical-Power Interactive Calculator"),
+  "Interactive Statistical-Power Calculator"),
   
   # Sidebar with a slider input for number of observations
   sidebarPanel(
@@ -31,14 +31,14 @@ shinyUI(pageWithSidebar(
     wellPanel(
       helpText("Null Distribution:"),
       sliderInput("m1", 
-                  "Mean value for null distribution, mu1:", 
+                  HTML('Mean value for null distribution, &mu;1'),
                   min = 0.0,
                   max = 2, 
                   value = 0., 
                   step = 0.1),
       br(),
       sliderInput("sd1", 
-                  "Standard deviation for null distribution, sd1:", 
+                  HTML('Standard deviation for null distribution, &sigma;1:'), 
                   min = 0.5,
                   max = 2, 
                   value = 1., 
@@ -46,14 +46,14 @@ shinyUI(pageWithSidebar(
     wellPanel(
       helpText("Sample Distribution:"),
       sliderInput("m2", 
-                  "Mean value for sample distribution, mu2:", 
+                  HTML('Mean value for sample distribution, &mu;2'), 
                   min = 2.1,
                   max = 5, 
                   value = 3., 
                   step = 0.1),
       br(),
       sliderInput("sd2", 
-                  "Standard deviation for sample distribution, sd2:", 
+                  HTML('Standard deviation for sample distribution, &sigma;2:'), 
                   min = 0.5,
                   max = 2, 
                   value = 1., 
@@ -61,8 +61,9 @@ shinyUI(pageWithSidebar(
     wellPanel(
       checkboxInput(inputId = "show_arrows",
                     label = strong("Show Labels"),
-                    value = TRUE),
-      br(),
+                    value = TRUE)
+    ),
+    wellPanel(
       downloadButton("savePlot", "Download PDF Graphic"),
       br(),
       br(),
